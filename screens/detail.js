71b6674bf950ca1d94a3a96f2d86bde8890ebbb1
atwanzireT <tatwanzire@gmail.com>
2022-11-19 04:41:26 +0300

    react native navigation params

--- screens/detail.js
+++ screens/detail.js
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
 
-export default function Detail() {
+export default function Detail({route}) {
+    const {itemId, otherParam} = route.params;
     return (
         <View>
             <Text>Detail</Text>
+            <Text>itemId: {JSON.stringify(itemId)}</Text>
+            <Text>otherParam: {JSON.stringify(otherParam)}</Text>
         </View>
     );
 }
--- screens/home.js
+++ screens/home.js
@@ -2,14 +2,35 @@ import { Text, View, Button } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function Home({navigation}) {
+function Home({ navigation }) {
   return (
     <View>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('detail')}
-      />
+      <View>
+        <Text>This is My</Text>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('detail',
+            {
+              itemId: 12,
+              otherParam: "Anything you want here",
+            }
+          )}
+        />
+      </View>
+      <View>
+        <Text>This is My</Text>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('detail',
+            {
+              itemId: 53,
+              otherParam: "Another thing you want here",
+            }
+          )}
+        />
+      </View>
     </View>
   );
 }
